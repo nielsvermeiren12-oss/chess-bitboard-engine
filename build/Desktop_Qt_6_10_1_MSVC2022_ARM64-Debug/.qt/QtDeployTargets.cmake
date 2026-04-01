a04e6a2301cedb7f1fc1a729b4666a57f3efd330
Niels Vermeiren <nielsvermeiren12@gmail.com>
2026-04-01 15:13:46 +0200

    remove build folder

--- build/Desktop_Qt_6_10_1_MSVC2022_ARM64-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChessGame_FILE C:/Projects/ChessEngine/build/Desktop_Qt_6_10_1_MSVC2022_ARM64-Debug/ChessGame.exe)
-set(__QT_DEPLOY_TARGET_ChessGame_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChessGame_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Quickd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Concurrentd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6OpenGLd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6QmlMetad.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6QmlModelsd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6QmlWorkerScriptd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Qmld.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Networkd.dll;C:/Qt/6.10.1/msvc2022_arm64/bin/Qt6Cored.dll)
